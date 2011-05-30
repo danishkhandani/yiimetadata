5f2a560edd554d5e9d56d9f88cc5a339f347e38c
vitaliy.step <vitaliy.step@2bd53bf3-e6c8-0c84-222a-30f7b3ebece0>
2011-05-30 10:50:12 +0000

    Some comments, code identing and refactored naming of private methods (also private become protected)

--- EMetadata.php
+++ EMetadata.php
@@ -3,24 +3,26 @@
 * EMetadata class file
 * @author Vitaliy Stepanenko <mail@vitaliy.in>
 * @version 0.2
+* @package metadata
 * @license BSD   
 */
 
 /**
-*  Metadata Helps to get metadata about models,controllers and actions in application* 
+*  Metadata Helps to get metainformation about models,controllers and actions in your application* 
 * 
 * For using you need:
-* 1. Place this file to directory with components of your application (your_app_dir/protected/components)
-* 2. Add it to 'components' in your application config (your_app_dir/protected/config/main.php)
+* 1. Place this file to components directory of your application (<your_app_dir>/protected/components) 
+* 2. Add to 'components' section in your application config (<your_app_dir>/protected/config/main.php) next lines:
 * 'components'=>array(
-*   'metadata'=>array('class'=>'ext.components.metadata.EMetadata'),
+*   'metadata'=>array('class'=>'EMetadata'),
 *    ...
 *  ),        
 * 3. Use:
 *   $user_actions = Yii::app()->metadata->getActions('UserController');
 *   var_dump($user_actions);
 */
-class EMetadata extends CApplicationComponent {
+class EMetadata extends CApplicationComponent
+{
 
     /**
     * Get all information about application
@@ -30,37 +32,33 @@ class EMetadata extends CApplicationComponent {
     public function getAll()
     {
         
-        $meta=array(
-            'models'=>$this->getModels(),
-            'controllers'=>$this->getControllers(),
-            'modules'=>$this->getModules(),
+        $meta = array(
+            'models'        => $this->getModels(),
+            'controllers'   => $this->getControllers(),
+            'modules'       => $this->getModules(),
         );
-        foreach ($meta['controllers'] as &$controller)
-        {
-            $controller=array(
-                'name'=>$controller,
-                'actions'=>$this->getActions($controller)
+        foreach ($meta['controllers'] as &$controller) {
+            $controller = array(
+                'name'      => $controller,
+                'actions'   => $this->getActions($controller)
             );   
         }
          
-        foreach ($meta['modules'] as &$module)
-        {
+        foreach ($meta['modules'] as &$module) {
             
-            $controllers=$this->getControllers($module);
+            $controllers = $this->getControllers($module);
                         
-            foreach ($controllers as &$controller)
-            {
-                $controller=array(
-                    'name'=>$controller,
-                    'actions'=>$this->getActions($controller,$module)
+            foreach ($controllers as &$controller) {
+                $controller = array(
+                    'name'      => $controller,
+                    'actions'   => $this->getActions($controller,$module)
                 );   
             }
-            
-            
-            $module=array(
-                'name'=>$module,
-                'controllers'=>$controllers,
-                'models'=>$this->getModels($module),
+                        
+            $module = array(
+                'name'          => $module,
+                'controllers'   => $controllers,
+                'models'        => $this->getModels($module),
             );
             
         }
@@ -72,32 +70,34 @@ class EMetadata extends CApplicationComponent {
     /**
     * Get actions of controller
     * 
-    * @param mixed $controller
-    * @param mixed $module
-    * @return mixed
+    * @param string|CController $controller
+    * @param string|null $module
+    * @return array
     */
     public function getActions($controller, $module=null)
     {
-        if ($module!=null){
-            $path=join(DIRECTORY_SEPARATOR,array(Yii::app()->modulePath,$module,'controllers'));
+        if ($controller instanceof CController) {
+            $controller = get_class($controller);
+        }
+        if ($module != null){
+            $path = join(DIRECTORY_SEPARATOR,array(Yii::app()->modulePath, $module, 'controllers'));
             $this->setModuleIncludePaths($module);
         }else{
-            $path='protected'.DIRECTORY_SEPARATOR.'controllers';
+            $path = 'protected' . DIRECTORY_SEPARATOR . 'controllers';
         }        
         
         
-            include_once($path.DIRECTORY_SEPARATOR.$controller.'.php');        
+            include_once($path . DIRECTORY_SEPARATOR . $controller . '.php');        
             $reflection = new ReflectionClass($controller); 
-            $methods = $reflection->getMethods(); 
+            $methods    = $reflection->getMethods(); 
         
         //$cInstance=new $controller(null);
         // var_dump($cInstance->actions());
-        $actions=array();
+        $actions = array();
         foreach($methods as $method)
         {           
-            if (strpos($method->name,'action')===0 and ctype_upper($method->name[6]))
-            {
-                $actions[]=str_replace('action','',$method->name);
+            if (strpos($method->name,'action')===0 and ctype_upper($method->name[6])) {
+                $actions[] = str_replace('action','',$method->name);
             }
         }
         return $actions;
@@ -107,13 +107,13 @@ class EMetadata extends CApplicationComponent {
     /**
     * Set php include paths for module
     * 
-    * @param mixed $module
+    * @param string $module
+    * @todo refactor it
     */
-    private function setModuleIncludePaths($module)
+    protected function setModuleIncludePaths($module)
     {       
         set_include_path(join(PATH_SEPARATOR,array(
-            get_include_path(),
-            //join(DIRECTORY_SEPARATOR,array(Yii::app()->modulePath,$module,'controllers')),
+            get_include_path(),            
             join(DIRECTORY_SEPARATOR,array(Yii::app()->modulePath,$module,'components')),
             join(DIRECTORY_SEPARATOR,array(Yii::app()->modulePath,$module,'models')),
             join(DIRECTORY_SEPARATOR,array(Yii::app()->modulePath,$module,'vendors')),
@@ -123,41 +123,38 @@ class EMetadata extends CApplicationComponent {
     /**
     * Get list of controllers with actions
     * 
-    * @param mixed $module
-    * @return array
+    * @param string|null $module
+    * @return array 
     */
     function getControllersActions($module=null)
     {
         $c=$this->getControllers($module);
-        foreach ($c as &$controller)
-        {
-            $controller=array(
-                'name'=>$controller,
-                'actions'=>$this->getActions($controller, $module)
+        foreach ($c as &$controller) {
+            $controller = array(
+                'name'      => $controller,
+                'actions'   => $this->getActions($controller, $module)
             );   
         }
         return $c;
     }
     
     /**
-    * Scans controller directory & return array of MVC controllers
+    * Scan controller directory & return array of MVC controllers
     * 
-    * @param mixed $module
-    * @param mixed $include_classes
+    * @param string|null $module    
     * @return array
     */
     public function getControllers($module=null)
     {
 
         if ($module!=null){
-            $path=join(DIRECTORY_SEPARATOR,array(Yii::app()->modulePath,$module,'controllers'));            
+            $path = join(DIRECTORY_SEPARATOR, array(Yii::app()->modulePath, $module, 'controllers'));            
         }else{
-            $path='protected'.DIRECTORY_SEPARATOR.'controllers';
+            $path = 'protected' . DIRECTORY_SEPARATOR . 'controllers';
         }                                
         $controllers = array_filter(scandir($path),array($this,'isController'));
-        foreach ($controllers as &$c)
-        {            
-            $c=str_ireplace('.php','',$c);
+        foreach ($controllers as &$c) {            
+            $c=str_ireplace('.php', '', $c);
         }       
         return $controllers;         
     }
@@ -165,29 +162,26 @@ class EMetadata extends CApplicationComponent {
     /**
     * Scans models directory & return array of MVC models
     * 
-    * @param mixed $module
-    * @param mixed $include_classes
+    * @param string|null $module
+    * @param bool $include_classes
     * @return array
     */
-    public function getModels($module=null,$include_classes=false)
+    public function getModels($module=null, $include_classes=false)
     {
 
-        if ($module!=null){
-            $path=join(DIRECTORY_SEPARATOR,array(Yii::app()->modulePath,$module,'models'));
-        }else{
-            $path='protected'.DIRECTORY_SEPARATOR.'models';
+        if ($module != null){
+            $path = join(DIRECTORY_SEPARATOR, array(Yii::app()->modulePath, $module, 'models'));
+        } else {
+            $path = 'protected'.DIRECTORY_SEPARATOR.'models';
         }                
 
-        $files = scandir($path);
-        $models=array();
-        foreach ($files as $f)
-        {
-            if (stripos($f,'.php')!==false)
-            {
-                $models[]=str_ireplace('.php','',$f);
-                if ($include_classes)
-                {
-                    include_once($path.DIRECTORY_SEPARATOR.$f);
+        $files  = scandir($path);
+        $models = array();
+        foreach ($files as $f) {
+            if (stripos($f,'.php') !== false) {
+                $models[] = str_ireplace('.php', '', $f);
+                if ($include_classes) {
+                    include_once($path . DIRECTORY_SEPARATOR . $f);
                 } 
 
             }
@@ -197,13 +191,13 @@ class EMetadata extends CApplicationComponent {
     }
 
     /**
-    * Used in getModules() to filter array of files & directories
+    * Not used
     * 
-    * @param mixed $a
+    * @param string $fileName
     */
-    private function isController($a)
+    private function isControllerFile($fileName)
     {
-        return stripos($a,'Controller.php')!==false;
+        return stripos($fileName,'Controller.php') !== false;
     }
 
 
@@ -214,20 +208,19 @@ class EMetadata extends CApplicationComponent {
     */
     public function getModules()
     {        
-        $modules = scandir(Yii::app()->modulePath);
-        // $modules=array_filter($modules,function($a) {return true;});
-        $modules=array_filter($modules,array($this,'isModule'));
+        $modules = scandir(Yii::app()->modulePath);        
+        $modules=array_filter($modules, array($this, 'isModuleFile'));
         return $modules;
     }    
 
     /**
     * Used in getModules() to filter array of files & directories
     * 
-    * @param mixed $a
+    * @param string $name
     */
-    private function isModule($a)
+    protected function isModuleFile($name)
     {
-        return $a!='.' and $a!='..' and is_dir(Yii::app()->modulePath.DIRECTORY_SEPARATOR.$a);
+        return $name!='.' and $name!='..' and is_dir(Yii::app()->modulePath . DIRECTORY_SEPARATOR . $name);
     }
 
 }
